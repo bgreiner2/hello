db64fd2820ebcdb4a60c8efae39f6e17479141cf
Ben Greiner <bgreiner2@huskers.unl.edu>
2022-08-25 17:40:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Ben Greiner
+ *         bgreiner2@huskers.unl.edu
+ * Date: 8/25/2022
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Ben, Computer Engineering\n");
+ //I was not enrolled in this class when Lab 1 was assigned, so I completed this on my own and did not have a partner
+
+  return 0;
+}
